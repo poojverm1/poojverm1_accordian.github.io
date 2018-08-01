50a7e724c5403c57edf307c6e2929de18fa25df1
Your Name <you@example.com>
2018-08-01 10:51:37 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,39 @@
+$(document).ready(() => {
+
+    // handle the click of the button
+
+    $(".img-top-one").click(() => {
+
+        $(".body-one").toggle();
+
+    }) 
+
+
+
+
+
+
+    $(".img-top-two").click(() => {
+
+        $(".body-two").toggle();
+
+    }) 
+
+
+	
+    $(".img-top-three").click(() => {
+
+        $(".body-three").toggle();
+
+    }) 
+
+
+
+
+
+
+
+
+
+
+})
